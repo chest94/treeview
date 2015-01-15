911753b863d937be00339df26a1b8e7ba8c1303a
DNDesarrollo <DNDesarrollo@DNDesarrollo-PC>
2015-01-15 17:13:08 -0600

    tree

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,29 +1,51 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-    class Welcome extends CI_Controller {
-	public function index()
-	{
-            $this->load->view('tree');
-            //$this->get_unidades();
-	}
-        
-        public function get_unidades()
-        {
-            $this->db->select('id_unidad as id, id_unidad_padre as parent, nombre_unidad as text');
-            $this->db->from('unidad');
-            $result = $this->db->get();
-            
-            $arreglo = array();
-            
-            foreach($result->result_array() as $row)
-            {
-                $arreglo[] = $row;
-            }
-            echo json_encode($arreglo);
-        }
-        
-        public function agregar()
-        {
-            
-        }
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+    public function index() {
+        $this->load->view('tree');
+        //$this->get_unidades();
     }
+
+    public function get_unidades() {
+        $this->db->select('id_unidad as id, id_unidad_padre as parent, nombre_unidad as text');
+        $this->db->from('unidad');
+        $result = $this->db->get();
+
+        $arreglo = array();
+
+        foreach ($result->result_array() as $row) {
+            $arreglo[] = $row;
+        }
+        echo json_encode($arreglo);
+    }
+
+    public function agregar() {
+       
+        if(isset($_POST) && !empty($_POST)){
+            $padre  = $_POST['padre'];
+            $nombre = $_POST['nombre'];
+    
+            if ($padre == 0) {
+                $padre = "#";
+            }
+
+            $unidad = array(
+                'nombre_unidad' => $nombre,
+                'id_unidad_padre' => $padre
+            );
+
+  
+            $this->db->insert('unidad', $unidad);
+        }
+       
+        
+        echo json_encode(array("msg"=>"fin"));
+    }
+
+}
+
 ?>
--- application/views/tree.php
+++ application/views/tree.php
@@ -14,6 +14,29 @@
         <script>
             $(function () {
                 generarArbol();
+                 $('#agregar').on('submit', function(){ 
+                    var nombre = $('#txtAgregar').val();
+                    var padre  = $('#dropuni').val();
+                    var data   = {
+                        "nombre": nombre,
+                        "padre" : padre
+                    };
+                    
+                    var uri = '<?= site_url() ?>';
+                        uri += "/welcome/agregar/";
+                    
+                    $.post(uri, data, function(resp){
+                        // hay algún error, porque no esta llegando aca
+                        //alert('a');
+                        // aca regargas el tree que para eso lo podemos meter en una funcion
+                        // justo aca puedes hacerlo
+                        
+                        $('#contenedor').html(""); // para vaciarlo
+                        var jsdiv = $('<div id="jstree"></div>');
+                        $('#contenedor').append(jsdiv);
+                        generarArbol();
+                    }, "json");
+                });
             });
             
             function generarArbol(){
@@ -31,18 +54,12 @@
                             //var tree = $("#jstree").jstree(true);
                             "items": function ($node) {
                                 return {
-                                    "Create": {
-                                        "separator_before": false,
-                                        "separator_after": true,
-                                        "label": "Crear",
-                                        "action": function (obj) {
-                                        }
-                                    },
                                     "Rename": {
                                         "separator_before": false,
                                         "separator_after": true,
                                         "label": "Renombrar",
                                         "action": function (obj) {
+                                            $node.id;
                                         }
                                     },
                                     "Remove": {
@@ -50,6 +67,26 @@
                                         "separator_after": false,
                                         "label": "Eliminar",
                                         "action": function (obj) {
+                                            // $node.id
+                                            // $node.tex, etc, justo acá
+                                            // esta es la de elimiacion
+                                            // sacas los datos del nodo y haces la magia con post
+                                            //y cómo valido que sea no sea papi de otros? es que en eso
+                                            //me perdí también porque yo quería mostrar un mensaje de exito 
+                                            //en el anterior y así, y si había un problema un GG xD
+                                            // pues en el anterior mandé el msg si te fijaste, solo que no hice nada con el mensaje
+                                            // en este caso, lo puedes validar de dos formas, del lado del cliente y del lado del server
+                                            /////// LADO DEL CLIENTE
+                                            // para esto tienes que ver primero la estructura del tree a ver como se forma, veamos
+                                            // bueno, por la estructura que tiene veo esto complicado xq tiene demasiadas cosas anidadas, pero 
+                                            // basicamente la idea era contar los elementos anidados con Jquery para saber los hijos, pero se nos va a complicar por la estructura
+                                            ////// LADO DEL SERVER
+                                            // hacer dos peticiones POST anidadas, es decir que una va a ir adentro del function(rep) de la otra
+                                            // la más externa toma el id del nodo y con una consulta en el server ves cuantos hijos tiene, si cero o más
+                                            // y so devuelves al cliente, que en base a ese número vos haces lo que quieres, si no quieres
+                                            // que elimine si tiene hijos mostras el mensaje, si no tiene hijos ejecutas el post interno que borraria el nodo
+                                            //eso va a estar guapo... xD para nada xD en un ratito se saca :p dijo el pro, bueno voy a ver primero lo del edit y después me meto con el 
+                                            //eliminar... chivo, y cualquier cosa me avisas :p
                                         }
                                     }
                                 };
@@ -62,37 +99,17 @@
                 }, "json");
             }
         </script>
-
-        <script>
-            $(function () {
-                $('#btnAgregar').click(function(){
-                    var nombre = $('#txtAgregar').val();
-                    var padre  = $('#dropuni').val();
-                    var data   = {
-                        "nombre": nombre,
-                        "padre" : padre
-                    };
-                    
-                    var uri = "algo";
-                    
-                    $.post(uri, data, function(resp){
-                        // aca regargas el tree que para eso lo podemos meter en una funcion
-                        $('#jstree').html(""); // para vaciarlo
-                        generarArbol();
-                    }, "json");
-                });
-            });
-        </script>
-
     </head>
     <body>
-        <div id="jstree">
+        <div id="contenedor">
+            <div id="jstree">
+            </div>
         </div>
-        <div id="agregar">
             <form id="agregar">
-                <input type="text" id="txtAgregar"> <br> 
+                <input type="text" name="Nombre" id="txtAgregar" required> <br> 
 
                 <?php
+                // O.o
                 $query = $this->db->query('select id_unidad, nombre_unidad '
                         . 'from unidad');
                 $dropdowns = $query->result();
@@ -100,13 +117,12 @@
                     $dropDownList[$dropdown->id_unidad] = $dropdown->nombre_unidad;
                 }
 
-                $finalDropDown = array_merge(array('#' => 'Indepentiende'), $dropDownList);
-                echo form_dropdown('unidad', $finalDropDown, 'id = "dropuni" class="textbox1" style="margin-top:10px;"');
+                //$finalDropDown = array_merge(array('0' => 'Indepentiende'), $dropDownList);
+                echo form_dropdown('unidad', $dropDownList,'' , 'id = "dropuni"');
                 ?>
                 <br>
 
-                <input type="button" id="btnAgregar" value="Agregar"> <br>
+                <input type="submit" id="btnAgregar" value="Agregar"> <br>
             </form>
-        </div>
     </body>
 </html>
